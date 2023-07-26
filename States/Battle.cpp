ddd84dde1fd9c2f1d860f6aaf05b07679079b69a
Daniel Sargeson <dansargeson@gmail.com>
2023-07-26 13:59:40 +0100

    removed some comments/obsolete code

--- States/Battle.cpp
+++ States/Battle.cpp
@@ -5,12 +5,6 @@
 
 Battle::Battle() : State(){
 
-
-    /*
-    EXPERMINTAL
-    */
-
-
     missCounter = 0;
     alpha = 255;
     alpha2 = 255;
@@ -25,10 +19,6 @@ Battle::Battle() : State(){
      std::uniform_int_distribution<int> numEnemiesDistribution(1, 3);
      noOfEnemies = numEnemiesDistribution(generator);
 
-    /*
-        BACK TO OLD WAY
-    */
-
     std::vector<std::string> ops;
     ops.push_back("Attack");
     ops.push_back("Defend");
@@ -41,15 +31,11 @@ Battle::Battle() : State(){
     enemyMenu = std::make_shared<GUI::Menu>(*StateData::GetInstance());
 
     StateData::GetInstance()->mainText = std::make_shared<GUI::Text>(Engine::GetInstance()->GetRenderer(), Engine::GetInstance()->GetWindow(), 5, 10, 12, 8, true);
-   // StateData::GetInstance()->mainText->setPosition(GUI::p2pX(5), GUI::p2pY(10));
-    //StateData::GetInstance()->mainText->setPositionF(5, 10, 12, 7);
     StateData::GetInstance()->mainText->setString("Test");
 
     StateData::GetInstance()->dynamicText = std::make_shared<GUI::Text>(Engine::GetInstance()->GetRenderer(), Engine::GetInstance()->GetWindow(), false);
     StateData::GetInstance()->dynamicText->setPosition(GUI::p2pX(20), GUI::p2pY(10));
     StateData::GetInstance()->dynamicText->setString("You are attacked: ");
-
-    //std::cout << StateData::GetInstance()->mainText->getString();
 
     StateData::GetInstance()->enemyText = std::make_shared<GUI::Text>(Engine::GetInstance()->GetRenderer(), Engine::GetInstance()->GetWindow(), true);
     StateData::GetInstance()->enemyText->setPosition(GUI::p2pX(20), GUI::p2pY(50));
@@ -67,7 +53,6 @@ Battle::Battle() : State(){
     enemyAttkTxt->clearText();
 
     playerTurn = false;
-//    playerAtk = false;
     endTurn = false;
 	choice = 0;
 	playerAccuracy = 0;
@@ -92,8 +77,6 @@ Battle::Battle() : State(){
 
      std::uniform_int_distribution<int> coinDistribution(1, 6);
      int coinToss = coinDistribution(generator);
-     //std::cout << std::endl << std::to_string(coinToss);
-	//int coinToss = rand() % 2 + 1;
 	if (coinToss <= 4)
 		playerTurn = true;
 	else
@@ -102,7 +85,6 @@ Battle::Battle() : State(){
 	escape = false;
 	playerDefeated = false;
 	enemyDefeated = false;
-//	noOfEnemies = rand() % 3 + 1;
 
 	for (size_t i = 0; i < noOfEnemies; i++){
 		enemies.push_back(Enemy(StateData::GetInstance()->getActiveCharacter()->getLevel() + rand()%5));
@@ -127,10 +109,8 @@ Battle::~Battle(){
     playerAttkTxt.reset();
 
     StateData::GetInstance()->mainText->clearText();
-    //StateData::GetInstance()->mainText->clearText();
     StateData::GetInstance()->mainText = std::make_shared<GUI::Text>(Engine::GetInstance()->GetRenderer(), Engine::GetInstance()->GetWindow(), 5, 5, 89, 60, true);
     if(!escape){
-      //  StateData::GetInstance()->dynamicText->clearText();
         StateData::GetInstance()->mainText->setString("Select an option: ");
     }
     else{
@@ -138,13 +118,6 @@ Battle::~Battle(){
     }
     StateData::GetInstance()->enemyText->clearText();
     StateData::GetInstance()->dynamicText->clearText();
-
-//    StateData::GetInstance()->mainText = std::make_shared<GUI::Text>(Engine::GetInstance()->GetRenderer(), Engine::GetInstance()->GetWindow(), false);
-//    StateData::GetInstance()->mainText->setPosition(GUI::p2pX(20), GUI::p2pY(20));
-//
-//    StateData::GetInstance()->enemyText = std::make_shared<GUI::Text>(Engine::GetInstance()->GetRenderer(), Engine::GetInstance()->GetWindow(), true);
-//    StateData::GetInstance()->enemyText->setPosition(GUI::p2pX(50), GUI::p2pY(20));
-//    StateData::GetInstance()->enemyText->setStringenemyText->setStringenemyText->setString("Select an option: ");
 }
 
 
@@ -162,7 +135,6 @@ if(!escape && !playerDefeated && !enemyDefeated) {
             ops.push_back("Use Item");
             battleMenu->setMenuOptions(ops, true);
             std::string msg = "HP: " + std::to_string(StateData::GetInstance()->getActiveCharacter()->getHP()) + "/" + std::to_string(StateData::GetInstance()->getActiveCharacter()->getHpMax());
-            //StateData::GetInstance()->mainText->setString(msg);
             std::string enemyMsg = "";
             if(battleMenu->getActive()){
 
@@ -172,7 +144,6 @@ if(!escape && !playerDefeated && !enemyDefeated) {
                 }
                 StateData::GetInstance()->enemyText->setString(enemyMsg);
             }
-			cout << "Choose action: \n";
 			}
         }
 
@@ -180,12 +151,9 @@ if(!escape && !playerDefeated && !enemyDefeated) {
 
             enemyAttacks();
         }
-
-        //endTurn = true;
 }
 
 
-//TODO: Update menu should be something like setMenuToEnemies...
 void Battle::updateMenu(){
 
 
@@ -239,15 +207,9 @@ if(playerDefeated){
             }
             StateData::GetInstance()->enemyText->setString(enemyMsg);
 
-    //############################START OF INIT BATTLE
     if(!escape && !playerDefeated && !enemyDefeated) {
 
 		if (playerTurn && !playerDefeated) {
-
-//            if(!playerTurn && !playerDefeated){
-//
-//                enemyAttacks();
-//            }
 
         //#############FINALISE TURN
             if (!StateData::GetInstance()->getActiveCharacter()->isAlive()) {
@@ -274,26 +236,19 @@ if(playerDefeated){
         }
 	}
 
-	//if(playerAttkTxt && enemyAttkTxt){
+
         updateText();
-	//}
 }
 
 void Battle::updateText(){
 
     if(playerAttkTxt && playerTurn){
 
-//      if(alpha > 5){
-//
-//            StateData::GetInstance()->dynamicText->render();
-//        }s
         if(alpha >= 0 && battleTxtTimer->GetDeltaTime() <= 3){
 
             alpha -= 5;
-            //SDL_Delay(200);
             SDL_SetTextureAlphaMod(playerAttkTxt->getTexture(), alpha);
             battleTxtTimer->Restart();
-          //  SDL_Delay(20);
         }
         else{
 
@@ -305,17 +260,10 @@ void Battle::updateText(){
 
     if(enemyAttkTxt){
 
-//      if(alpha > 5){
-//
-//            StateData::GetInstance()->dynamicText->render();
-//        }
-
         if(alpha2 >= 0){
 
             alpha2 -= 5;
-            //SDL_Delay(200);
             SDL_SetTextureAlphaMod(enemyAttkTxt->getTexture(), alpha2);
-         //   SDL_Delay(20);
         }
         else{
 
@@ -337,8 +285,6 @@ void Battle::playerAttacks(){
              std::uniform_int_distribution<int> playerTotalDistribution(1, playerTotal);
              combatRollPlayer = playerTotalDistribution(generator);
 
-           //  seed = (unsigned int)std::chrono::system_clock::now().time_since_epoch().count();
-           //  generator.seed(seed);
              std::uniform_int_distribution<int> enemyTotalDistribution(1, enemyTotal);
              combatRollEnemy = enemyTotalDistribution(generator);
 
@@ -347,7 +293,6 @@ void Battle::playerAttacks(){
 					//HIT
 					missCounter = 0;
 					damage = StateData::GetInstance()->getActiveCharacter()->getDamage();
-					//std::cout << "DAMGE: " << damage << " >>>>>>>>>>>>> ";
 					int totalDmg = enemies[choice].takeDamage(damage);
 					if(totalDmg < 0){
                         totalDmg = 1;
@@ -367,7 +312,6 @@ void Battle::playerAttacks(){
 						endMsg += " EXP Gained: " + std::to_string(gainEXP);
 
 						//ITEM ROLL
-						//TODO: CHANGE THIS IT DROPS SOMETHING EVERYTIME...
 						seed = (unsigned int)std::chrono::system_clock::now().time_since_epoch().count();
 
                         generator.seed(seed);
@@ -423,8 +367,6 @@ void Battle::playerAttacks(){
 							}
                         StateData::GetInstance()->dynamicText->setString(endMsg, true, GUI::p2pY(80));
 						enemies.erase(enemies.begin() + choice);
-						cout << "Continue...\n";
-						//cin.get();
 					} //####END PLAYER WINS
 				}
 				else {
@@ -436,7 +378,6 @@ void Battle::playerAttacks(){
 				}
 
 				battleGameTimer->start();
-				//endTurn = true;
 }
 
 void Battle::enemyAttacks(){
@@ -451,21 +392,16 @@ void Battle::enemyAttacks(){
 
     if(!playerTurn && !escape && !enemyDefeated){
 			//ENEMY TURN
-			//cout << "= ENEMY ATTACKS = \n\n";
 			for (size_t i = 0; i < enemies.size(); i++){    //TIMER NEEDS TO GO AROUND HERE....
 				combatTotal = enemies[i].getAccuracy() + StateData::GetInstance()->getActiveCharacter()->getDefence();
 				enemyTotal = enemies[i].getAccuracy() / (double)combatTotal * 100;
 				playerTotal = StateData::GetInstance()->getActiveCharacter()->getDefence() / (double)combatTotal * 100;
-				//combatRollPlayer = rand() % playerTotal + 1;
-				//combatRollEnemy = rand() % enemyTotal + 1;
 
 				seed = (unsigned int)std::chrono::system_clock::now().time_since_epoch().count();
              generator.seed(seed);
              std::uniform_int_distribution<int> playerTotalDistribution(1, playerTotal);
              combatRollPlayer = playerTotalDistribution(generator);
 
-             //seed = (unsigned int)std::chrono::system_clock::now().time_since_epoch().count();
-             //generator.seed(seed);
              std::uniform_int_distribution<int> enemyTotalDistribution(1, enemyTotal);
              combatRollEnemy = enemyTotalDistribution(generator);
 
@@ -476,8 +412,6 @@ void Battle::enemyAttacks(){
 					enemyMsg += "\n" + enemies[i].getName() + " HIT for " + std::to_string(totalDmg) + " damage! ";
 					alpha2 = 255;
 					enemyAttkTxt->setString(enemyMsg, true, GUI::p2pX(80));
-					//std::string playMsg = "HP: " + std::to_string(StateData::GetInstance()->getActiveCharacter()->getHP()) + "/" + std::to_string(StateData::GetInstance()->getActiveCharacter()->getHpMax());
-					enemyAttkTxt->setString(enemyMsg, true, GUI::p2pX(80));
 					if (!StateData::GetInstance()->getActiveCharacter()->isAlive()) {
 						cout << "YOU DIED!\n\n";
 						playerDefeated = true;
@@ -486,23 +420,17 @@ void Battle::enemyAttacks(){
 				}
 				else {
 					//MISS
-					//enemyAttkTxt->setPosition(GUI::p2pX(20), GUI::p2pY(50));
 					enemyMsg += "\n" + enemies[i].getName() + " Missed! ";
 					alpha2 = 255;
 					enemyAttkTxt->setString(enemyMsg, true, GUI::p2pX(80));
 					cout << "ENEMY MISSED!\n\n";
 				}
-
-				cout << "Continue...\n";
-				//fcin.get();
 			}
 
             std::string msg = "HP: " + std::to_string(StateData::GetInstance()->getActiveCharacter()->getHP()) + "/" + std::to_string(StateData::GetInstance()->getActiveCharacter()->getHpMax());
             StateData::GetInstance()->mainText->setString(msg);
 			//END TURN
-			//playerTurn = true;
 			 endTurn = true;
-			 //std::cout << endl << endl << "END TURN: " << endTurn << " playerTurn: " << playerTurn << endl;
 		}
 }
 
